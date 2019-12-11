07d5780bfdad5e642e56c9c369b9c03a4e110b1a
Daniel Dawson <dannyldawson@gmail.com>
2019-12-11 00:18:00 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-
   handleOnClick() {
     this.props.dispatch({
-      type: 'INCREASE_COUNT',
+      type: "INCREASE_COUNT"
     });
   }
 
   render() {
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick()}>
-          Click
-        </button>
+        <button onClick={event => this.handleOnClick()}>Click me bitch</button>
         <p>{this.props.items.length}</p>
       </div>
     );
   }
-};
+}
 
 export default App;
